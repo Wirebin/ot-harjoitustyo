3b4f01b3b0dadadee2f9a244258db6f13e0f9053
Wirebin <107724483+Wirebin@users.noreply.github.com>
2025-04-08 22:52:07 +0300

    Add text for button

--- src/button.py
+++ src/button.py
@@ -1,5 +1,5 @@
-import pygame
 from component import Component
+import pygame
 
 class Button(Component):
     def __init__(self, location: tuple, size: tuple, color_normal: tuple, color_hover: tuple, button_text, on_click=None):
@@ -10,7 +10,10 @@ class Button(Component):
         
         self.button_surface = pygame.Surface((self.size[0], self.size[1]))
         self.button_rect = pygame.Rect(self.location[0], self.location[1], self.size[0], self.size[1])
-        self.button_text = button_text
+        
+        self.font = pygame.font.Font(None, self.button_rect.height - 10)
+        self.button_text = self.font.render(button_text, True, (0,0,0))
+        self.button_text_rect = self.button_text.get_rect(center=self.button_rect.center)
 
         self.on_click = on_click
 
@@ -20,9 +23,12 @@ class Button(Component):
             self.on_click()
 
     def draw(self, screen, mouse_pos):
-        self.button_surface.fill(self.color_normal)
         # If hovering over button
         if self.button_rect.collidepoint(mouse_pos):
-            self.button_surface.fill(self.color_hover)
-        
-        screen.blit(self.button_surface, self.button_rect)
+            pygame.draw.rect(screen, self.color_hover, self.button_rect)
+        # Not hovering over
+        else:
+            pygame.draw.rect(screen, self.color_normal, self.button_rect)
+
+        # Draw text
+        screen.blit(self.button_text, self.button_text_rect)
