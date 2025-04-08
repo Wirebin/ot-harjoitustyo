840d3bf8940420b95f5d358f56bb75531b0ad8aa
Wirebin <107724483+Wirebin@users.noreply.github.com>
2025-04-08 23:02:19 +0300

    Add game state functionality

---
+++ src/state_manager.py
@@ -0,0 +1,23 @@
+from states import Game_States
+
+class State_Manager:
+    def __init__(self):
+        self.current_state = Game_States.MENU
+        self.state_amount = len(Game_States)
+
+
+    def next_state(self):
+        # If state value is over the limit, go back to the first one.
+        if self.current_state.value >= self.state_amount:
+            self.current_state = Game_States.MENU
+
+        # Otherwise increase state by one / move to the next state.
+        else:
+            self.current_state = Game_States(self.current_state.value + 1)
+
+
+    # def go_to_state(self, state: Game_States):
+    #     # If specified state exists, set it as current.
+    #     if state in Game_States:
+    #         self.current_state = state
+
---
+++ src/states.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Game_States(Enum):
+    MENU = 1
+    GAME = 2
+    RESULT = 3
