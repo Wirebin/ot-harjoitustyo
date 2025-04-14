aa6f0a08a77399792394b88c6770e197d4574435
Wirebin <107724483+Wirebin@users.noreply.github.com>
2025-04-15 02:06:53 +0300

    Remove colors from parameters
    Removed the normal and hover button colors from the parameters to please pylint.

--- src/button.py
+++ src/button.py
@@ -1,34 +1,42 @@
-from component import Component
 import pygame
+from component import Component
 
 class Button(Component):
-    def __init__(self, location: tuple, size: tuple, color_normal: tuple, color_hover: tuple, button_text, on_click=None):
+    def __init__(self,
+                 location: tuple,
+                 size: tuple,
+                 button_text: str,
+                 on_click=None):
+
         self.location = location
         self.size = size
-        self.color_normal = color_normal
-        self.color_hover = color_hover
-        
-        self.button_rect = pygame.Rect(self.location[0], self.location[1], self.size[0], self.size[1])
-        
+
+        self.button_rect = pygame.Rect(self.location[0],
+                                       self.location[1],
+                                       self.size[0],
+                                       self.size[1])
+
         self.button_text = button_text
-        self.font = pygame.font.Font(None, self.button_rect.height - 10)
-        self.button_text_render = self.font.render(button_text, True, (0,0,0))
+        font = pygame.font.Font(None, self.button_rect.height - 10)
+        self.button_text_render = font.render(button_text, True, (0,0,0))
         self.button_text_rect = self.button_text_render.get_rect(center=self.button_rect.center)
 
         self.on_click = on_click
 
-    def update(self, mouse_pos):
+    def update(self):
         # If left-clicked on top of button
-        if self.button_rect.collidepoint(mouse_pos) and pygame.mouse.get_pressed(num_buttons=3)[0]:
+        if self.button_rect.collidepoint(pygame.mouse.get_pos()) and \
+            pygame.mouse.get_pressed(num_buttons=3)[0]:
             self.on_click()
 
-    def draw(self, screen, mouse_pos):
+    def draw(self, screen):
         # If hovering over button
-        if self.button_rect.collidepoint(mouse_pos):
-            pygame.draw.rect(screen, self.color_hover, self.button_rect)
+        if self.button_rect.collidepoint(pygame.mouse.get_pos()):
+            pygame.draw.rect(screen, (150, 150, 150), self.button_rect)
         # Not hovering over
         else:
-            pygame.draw.rect(screen, self.color_normal, self.button_rect)
+            pygame.draw.rect(screen, (200, 200, 200), self.button_rect)
 
         # Draw text
         screen.blit(self.button_text_render, self.button_text_rect)
+        
--- src/tests/button_test.py
+++ src/tests/button_test.py
@@ -5,7 +5,7 @@ from button import Button
 class TestButton(unittest.TestCase):
     def setUp(self):
         pygame.init()
-        self.button = Button((100,100), (100, 50), (255,255,255), (0,0,0), "Test")
+        self.button = Button((100,100), (100, 50), "Test")
 
     def test_correct_location(self):
         self.assertEqual(self.button.location, (100, 100))
@@ -15,9 +15,3 @@ class TestButton(unittest.TestCase):
 
     def test_correct_text(self):
         self.assertEqual(self.button.button_text, "Test")
-
-    def test_correct_normal_color(self):
-        self.assertEqual(self.button.color_normal, (255,255,255))
-
-    def test_correct_hover_color(self):
-        self.assertEqual(self.button.color_hover, (0,0,0))
